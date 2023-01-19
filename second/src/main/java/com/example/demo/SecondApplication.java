31a582b511ffa0189527eb025a04cdce87a8981f
HyunJoon_Cho <9hjcho2@gmail.com>
2023-01-19 21:24:00 +0900

    commit20230119

---
+++ second/src/main/java/com/example/demo/SecondApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication //내가 설정클래스 파일
+@ComponentScan
+@ComponentScan(basePackages = "upload0118_1")
+@ComponentScan(basePackages = "spring.mybatis0119_1")
+@MapperScan(basePackages = "spring.mybatis0119_1")
+@ComponentScan(basePackages = "websocket0119_2")
+public class SecondApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApplication.class, args);
+		
+	}
+}
